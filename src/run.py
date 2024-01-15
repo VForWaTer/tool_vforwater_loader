c16670373dbc8663a9540ff85a60988e56f901ae
Mirko Mälicke <mirko.maelicke@kit.edu>
2024-01-15 12:12:36 +0100

    dummy implementation

--- src/run.py
+++ src/run.py
@@ -1,6 +1,5 @@
 import os
 from datetime import datetime as dt
-from pprint import pprint
 
 from json2args import get_parameter
 
@@ -8,16 +7,43 @@ from json2args import get_parameter
 kwargs = get_parameter()
 
 # check if a toolname was set in env
-toolname = os.environ.get('TOOL_RUN', 'foobar').lower()
+toolname = os.environ.get('TOOL_RUN', 'vforwater_loader').lower()
 
-# switch the tool
-if toolname == 'foobar':
-    # RUN the tool here and create the output in /out
-    print('This toolbox does not include any tool. Did you run the template?\n')
-    
-    # write parameters to STDOUT.log
-    pprint(kwargs)
-
-# In any other case, it was not clear which tool to run
-else:
+# raise an error if the toolname is not valid
+if toolname != 'vforwater_loader':
     raise AttributeError(f"[{dt.now().isocalendar()}] Either no TOOL_RUN environment variable available, or '{toolname}' is not valid.\n")
+
+
+# extract the dataset ids
+dataset_ids = kwargs['dataset_ids']
+
+# extract the optional information
+start = kwargs.get('start_date')
+end = kwargs.get('end_date')
+reference_area = kwargs.get('reference_area')
+
+
+# ----------------------------
+# FROM HERE, ONLY DEVELOPMENT
+
+# build the message for now
+MSG = f"""
+This is the V-FOR-WaTer data loader. Ufortunately, this tool is not yet implemented.
+
+The following information has been submitted to the tool:
+
+START DATE:         {start}
+END DATE:           {end}
+REFERENCE AREA:     {reference_area is None}
+
+DATASET IDS:
+{', '.join(map(str, dataset_ids))}
+
+"""
+
+# RUN the tool here and create the output in /out
+print(MSG)
+
+# save
+with open('/out/result.txt', 'w') as f:
+    f.write(MSG)
